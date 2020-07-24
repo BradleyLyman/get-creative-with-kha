0c8eded8276a391840a8ecd25b2bdbba5b60b0e0
Bradley Lyman <lyman.brad3211@gmail.com>
2020-07-23 18:42:30 -0700

    move epicycle behavior into separate file

--- P3-Signals/1-Epicycles/src/App.hx
+++ P3-Signals/1-Epicycles/src/App.hx
@@ -1,52 +1,24 @@
 package;
 
-import kha.graphics2.Graphics;
-import kha.math.FastVector2;
 import kha.Framebuffer;
 
-using Math;
+using Epicycle;
 using support.FloatOps;
-
-@:structInit
-class Epicycle {
-  public var phase:Float;
-  public var offset:Float;
-  public var amplitude:Float;
-
-  public var center:FastVector2;
-
-  public function new(phase:Float, offset:Float, amplitude:Float) {
-    this.phase = phase;
-    this.offset = offset;
-    this.amplitude = amplitude;
-    this.center = {x: 0, y: 0};
-  }
-
-  public function sample(t:Float):FastVector2 {
-    final x = Math.cos((t * phase) + offset) * amplitude;
-    final y = Math.sin((t * phase) + offset) * amplitude;
-    return center.add({x: x, y: y});
-  }
-
-  public function draw(g2:Graphics, segments:Int = 32) {
-    var prevX = center.x + amplitude;
-    var prevY = center.y;
-    for (i in 1...segments) {
-      final angle = (i / segments).lerp(0, Math.PI * 2);
-      final x = center.x + angle.cos() * amplitude;
-      final y = center.y + angle.sin() * amplitude;
-      g2.drawLine(prevX, prevY, x, y);
-      prevX = x;
-      prevY = y;
-    }
-    g2.drawLine(prevX, prevY, center.x + amplitude, center.y);
-  }
-}
 
 class App {
   var t:Float = 0.0;
+  var cycles:Array<Epicycle> = [];
 
-  public function new() {}
+  public function new() {
+    for (i in 1...20) {
+      cycles.push({
+        phase: (i / 20).lerp(0, Math.PI * 2),
+        offset: Math.random() * i,
+        amplitude: i * 2
+      });
+    }
+    cycles.sort((a, b) -> Math.round(b.amplitude - a.amplitude));
+  }
 
   /**
     The Kha scheduler attempts to run this every 16 milliseconds so assume a
@@ -62,14 +34,26 @@ class App {
     have exactly 1 frame.
   **/
   public function render(framebuffers:Array<Framebuffer>):Void {
-    final g2 = framebuffers[0].g2;
+    final screen = framebuffers[0];
+    final g2 = screen.g2;
     g2.begin();
 
-    final c:Epicycle = {phase: 2, offset: 0, amplitude: 100};
-    c.center = {x: framebuffers[0].width / 2, y: framebuffers[0].height / 2};
-    c.draw(g2, 64);
-    final sample = c.sample(t);
-    g2.drawLine(c.center.x, c.center.y, sample.x, sample.y);
+    // propagate centers at time t
+    cycles.propagateCenters({x: screen.width / 2, y: screen.height / 2}, t);
+
+    // draw lines
+    for (i in 1...cycles.length) {
+      final last = cycles[i - 1].center;
+      final now = cycles[i].center;
+      g2.drawLine(last.x, last.y, now.x, now.y, 2);
+    }
+    final last = cycles[cycles.length - 1].center;
+    final now = cycles[cycles.length - 1].sample(t);
+    g2.drawLine(last.x, last.y, now.x, now.y, 2);
+
+    for (cycle in cycles) {
+      cycle.draw(g2, 64);
+    }
 
     g2.end();
   }
